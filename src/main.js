e3fc35de0337062de9754aadc14f8489aa06834b
AcelisWeaven <jeremy.graziani@gmail.com>
2020-07-17 23:14:14 +0200

    Add anonymous analytics

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
 import App from './App.vue'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+Vue.use(VueAnalytics, {
+  id: process.env.VUE_APP_GA_ID,
+  set: [
+    { field: 'anonymizeIp', value: true }
+  ]
+})
 
 new Vue({
   render: h => h(App),
